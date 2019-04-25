36a1b0f47b66b93c355e242482e3539072885c4a
TomasRode <39487154+TomasRode@users.noreply.github.com>
2019-04-25 20:43:33 +0200

    Update hermit.m

--- hermit.m
+++ hermit.m
@@ -2,7 +2,7 @@ function [P] = hermit(X, F, dF)
   
   for i = 1:length(X)-1
     delta = (F(i+1)-F(i))/(X(i+1) - X(i));
-    P(:,i) = [F(i); dF(i); (delta - dF(i))/(X(i+1) - X(i)); (dF(i+1) - 2 * delta - dF(i))/(X(i+1) - X(i))^2 ];
+    P(:,i) = [F(i); dF(i); (delta - dF(i))/(X(i+1) - X(i)); (dF(i+1) - 2 * delta + dF(i))/(X(i+1) - X(i))^2 ];
   endfor 
   
   
