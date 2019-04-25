ac73bc89bb1ff46320130f43a24c3c125070b8bc
TomasRode <39487154+TomasRode@users.noreply.github.com>
2019-04-25 15:01:37 +0200

    Add files via upload

---
+++ DN14.m
@@ -0,0 +1,32 @@
+% DN14
+n = 6;
+X = linspace(0, 1, n+1);
+f = @(x) exp(-x + sin(10*x));
+df1 = @(x)(-1+ 10*cos(10*x));
+F = f(X);
+dF = F .* df1(X);
+
+tocke = repelem(X,2);
+P = hermit(X, F, dF);
+
+horner(tocke(1:4),P(:,1),0.2);
+
+z = linspace(0,1,70);
+w = f(z);
+
+hold on
+
+plot(z,w)
+
+st_tock = 20;
+for i = 1:n
+  v = linspace(X(i), X(i+1), st_tock);
+  %2*i-1,2*i-1+3
+  vrednosti = [];
+  for k = 1 : st_tock
+    vrednosti(k) = horner(tocke(2*i-1:2*i+2),P(:,i),v(k));
+  endfor
+  plot(v,vrednosti)
+endfor
+
+hold off
---
+++ horner.m
@@ -0,0 +1,8 @@
+function [p] = horner(X, diference, tocka)
+  a = diference;
+  n = length(X); 
+  p = a(n);               
+  for k = 1:n-1;
+      p = a(n-k) + (tocka - X(n-k))*p;
+  end
+endfunction
